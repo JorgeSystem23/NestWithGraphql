1f08b507ae545dff6b59b94d70c241dc66a720ad
JorgeSystem23 <jgrullon@popoyan.com>
2024-04-12 08:26:43 -0600

    Generate random numbers and integrate apollo server

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,22 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Float, Int, Query, Resolver } from "@nestjs/graphql";
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: "Hola Mundo es lo que retorna",
+    name: "hello",
+  })
   helloWorld(): string {
     return "Hello World";
+  }
+
+  @Query(() => Float, { name: "RandomNumber" })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, { name: "randomNumberToZero" })
+  getRandomNumberToZero(): number {
+    return Math.floor(Math.random() * 10);
   }
 }
